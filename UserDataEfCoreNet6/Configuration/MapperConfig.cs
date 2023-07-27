43dbdb237ed012d754475fb69e7a694653e00e83
Atahan Çelebi <atahancelebi98@gmail.com>
2023-07-27 17:28:04 +0300

    Configurations added with AutoMapper

---
+++ UserDataEfCoreNet6/Configuration/MapperConfig.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using AutoMapper;
+using UserDataEfCoreNet6.Data;
+using UserDataEfCoreNet6.Models.Email;
+using UserDataEfCoreNet6.Models.Phone;
+using UserDataEfCoreNet6.Models.User;
+
+namespace UserDataEfCoreNet6.Configuration
+{
+	public class MapperConfig: Profile
+	{
+		public MapperConfig()
+		{
+            CreateMap<User, UserDto>().ReverseMap();
+            CreateMap<Phone, PhoneDto>().ReverseMap();
+            CreateMap<Email, EmailDto>().ReverseMap();
+        }
+	}
+}
+
